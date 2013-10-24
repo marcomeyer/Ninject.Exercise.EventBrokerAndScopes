f967fbc1d36fdaf960b76db84cdcfa0d1fdf1d6a
marcomeyer <marcomeyer@hispeed.ch>
2013-10-24 18:22:54 +0200

    Temporary fix: Holding EditorPresenters so GC won't collect them

--- EventBrokerAndScopes/MainForm.cs
+++ EventBrokerAndScopes/MainForm.cs
@@ -1,21 +1,30 @@
 ﻿namespace EventBrokerAndScopes
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
     using System.Windows.Forms;
+
+    using EventBrokerAndScopes.Editor;
 
     public partial class MainForm : Form
     {
         private readonly IEditorFactory editorFactory;
 
+        private readonly IList<IEditorPresenter> editors;
+
         public MainForm(IEditorFactory editorFactory)
         {
             this.editorFactory = editorFactory;
+            this.editors = new List<IEditorPresenter>();
             InitializeComponent();
         }
 
         private void BtnCreateEditorClick(object sender, EventArgs e)
         {
-            this.editorFactory.Create().Show();
+            IEditorPresenter editor = this.editorFactory.Create();
+            this.editors.Add(editor);
+            editor.Show();
         }
 
         private void BtnGCCollect(object sender, EventArgs e)
